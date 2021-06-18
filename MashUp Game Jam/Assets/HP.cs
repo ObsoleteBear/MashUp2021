3efeb5e70e24fe4e1e3c5ef1a6be50c4b269f4f9
V1660 <86119539+V1660@users.noreply.github.com>
2021-06-18 18:20:49 -0500

    Enemy AI + HP,  iFrames, and Death

---
+++ MashUp Game Jam/Assets/HP.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+   
+
+    public class HP : MonoBehaviour
+{
+    public float lastHit;
+    public float iFrames = 10;
+    public bool playerIsDead = false;
+    public Rigidbody2D rb;
+    public int dmg;
+    public PlayerMovement playerMovement;
+
+
+    public int Health = 100; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health < 1)
+        {
+            playerIsDead = true;
+           
+        }
+        if (playerIsDead==true)
+        {
+            playerMovement.enabled = false;
+            Destroy(gameObject, 2);
+        }
+    }
+    public void OnTriggerStay2D (Collider2D trigger)
+    {
+        if (trigger.gameObject.tag == "Enemies")
+        {
+            if (lastHit < Time.time - iFrames)
+            {
+                Health = Health -dmg;
+                lastHit = Time.time;
+            }
+
+        }
+
+    
+        
+    }
+}
---
+++ MashUp Game Jam/Assets/followPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+     
+{
+    public float speed;
+    public Vector2 playerPosition;
+    public Vector2 position;
+    public GameObject player; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = gameObject.transform.position;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = new Vector2(player.transform.position.x, transform.position.y);
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, playerPosition, step);
+    }    
+
+ 
+}
