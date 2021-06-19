299c6de0f986737689852fcf921ee18be8ff626a
V1660 <86119539+V1660@users.noreply.github.com>
2021-06-19 12:54:38 -0500

    flowey

--- MashUp Game Jam/Assets/followPlayer.cs
+++ MashUp Game Jam/Assets/followPlayer.cs
@@ -3,24 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class followPlayer : MonoBehaviour
-     
+
 {
     public float speed;
     public Vector2 playerPosition;
     public Vector2 position;
-    public GameObject player; 
-
+    public GameObject player;
+    public Rigidbody2D rb;
+    public Vector2 jump;
+    public float jumpForce;
+    public bool hasJump;
+    public float playerHeight;
     // Start is called before the first frame update
     void Start()
     {
         position = gameObject.transform.position;
-       
 
+        rb = GetComponent<Rigidbody2D>();
+
+        jump = new Vector2(0, jumpForce);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.transform.position.y > transform.position.y + playerHeight)
+        {
+
+            if (hasJump == true)
+            {
+                rb.AddForce(jump);
+            }
+        }
+
         if (player == null)
         {
 
@@ -31,7 +46,27 @@ public class followPlayer : MonoBehaviour
         }
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, playerPosition, step);
-    }    
 
- 
+
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            hasJump = true;
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            hasJump = false;
+        }
+    }
+
+
+
+
 }
